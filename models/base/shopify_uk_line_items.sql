5af550bcd452a89b6b1f0a0def5705c2012f1335
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-07-27 17:10:54 +0200

    Update shopify_uk_line_items.sql

--- models/base/shopify_uk_line_items.sql
+++ models/base/shopify_uk_line_items.sql
@@ -1,8 +1,15 @@
 {%- set schema_name,
+        order_table_name,
         item_table_name, 
-        item_fund_table_name
-        = 'shopify_raw_uk', 'order_line', 'order_line_refund' -%}
+        item_fund_table_name,
+        shop_table_name
+        = 'shopify_raw_uk', 'order', 'order_line', 'order_line_refund', 'shop' -%}
 
+{%- set order_selected_fields = [
+    "id",
+    "created_at"
+] -%}
+        
 {%- set item_selected_fields = [
     "order_id",
     "id",
@@ -28,12 +35,45 @@
     "subtotal"
 ] -%}
 
+{%- set shop_selected_fields = [
+    "currency"
+] -%}
+
+{%- set order_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw_uk%', 'order') -%}
 {%- set order_line_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw_uk%', 'order_line') -%}
 {%- set order_line_refund_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw_uk%', 'order_line_refund') -%}
+{%- set shop_raw_tables = dbt_utils.get_relations_by_pattern('shopify_raw_uk%', 'shop') -%}
 
-WITH order_line_raw_data AS 
+WITH 
+    {% if var('currency') == 'USD' -%}
+    shop_raw_data AS 
+    ({{ dbt_utils.union_relations(relations = shop_raw_tables) }}),
+        
+    currency AS
+    (SELECT date, conversion_rate
+    FROM shop_raw_data LEFT JOIN utilities.currency USING (currency)
+    WHERE date <= current_date),
+    {%- endif -%}
+
+    {%- set conversion_rate = 1 if var('currency') != 'USD' else 'conversion_rate' %}    
+        
+    order_line_raw_data AS 
     ({{ dbt_utils.union_relations(relations = order_line_raw_tables) }}),
 
+    order_raw_data AS 
+    ({{ dbt_utils.union_relations(relations = order_raw_tables) }}),
+
+    orders AS 
+    (SELECT 
+
+        {% for field in order_selected_fields -%}
+        {{ get_shopify_clean_field(order_table_name, field)}}
+        {%- if not loop.last %},{% endif %}
+        {% endfor %}
+
+    FROM order_raw_data
+    ),
+        
     items AS 
     (SELECT 
 
@@ -68,9 +108,39 @@ WITH order_line_raw_data AS
     GROUP BY order_line_id
     )
 
-SELECT *,
+{%- set item_fields = [
+    "order_line_id",
+    "order_id",
+    "product_id",
+    "variant_id",
+    "product_title",
+    "variant_title",
+    "item_title",    
+    "price",
+    "quantity",
+    "sku",
+    "fulfillable_quantity",
+    "fulfillment_status",
+    "gift_card",
+    "index",
+    "refund_quantity",
+    "refund_subtotal" 
+] -%}
+        
+SELECT 
+    {%- for field in item_fields -%}
+        {%- if ('price' in field or 'refund' in field) %}
+        "{{ field }}"::float*{{ conversion_rate }}::float as "{{ field }}",
+        {%- else %}
+        "{{ field }}",
+        {%- endif -%}
+    {%- endfor %}
     quantity - refund_quantity as net_quantity,
     price * quantity - refund_subtotal as net_subtotal,
     order_line_id as unique_key
 FROM items 
 LEFT JOIN refund USING(order_line_id)
+{%- if var('currency') == 'USD' %}
+    LEFT JOIN orders USING (order_id)
+    LEFT JOIN currency ON orders.created_at::date = currency.date
+{%- endif %}
